40ad5c5bdbbdb2bf8b2c69e43a9cabd14bb13b68
Paul Yudkin <arito@Pauls-MacBook-Pro.local>
2025-04-10 20:58:13 +0300

    changes

--- runners/train_eval_runner.py
+++ runners/train_eval_runner.py
@@ -46,8 +46,8 @@ if __name__ == '__main__':
     model = get_model(10)
     #train_df, test_df = get_train_test(args.labels_file, args.data_folder, args.output_folder)
 
-    train_df = pd.read_pickle(os.path.join(args.output_folder, "train_features.pkl")).head(100)
-    test_df = pd.read_pickle(os.path.join(args.output_folder, "test_features.pkl")).head(100)
+    train_df = pd.read_pickle(os.path.join(args.output_folder, "train_features.pkl")).head(250)
+    test_df = pd.read_pickle(os.path.join(args.output_folder, "test_features.pkl")).head(250)
 
     train_df['table_range'] = train_df['table_range'].apply(ast.literal_eval)
     test_df['table_range'] = test_df['table_range'].apply(ast.literal_eval)
@@ -71,7 +71,7 @@ if __name__ == '__main__':
 
 
     optimizer = optim.SGD(model.parameters(), lr=0.005, momentum=0.9, weight_decay=0.0005)
-    train_model(model, train_loader, optimizer, 10, device)
+    train_model(model, train_loader, optimizer, args.epochs_number, device)
 
 
     evaluate_model(model, test_loader, device)

fab782181b42431c17c084ab3e7ee72d20233099
Paul Yudkin <arito@Pauls-MacBook-Pro.local>
2025-07-23 20:11:42 +0300

    d

--- runners/train_eval_runner.py
+++ runners/train_eval_runner.py
@@ -48,7 +48,7 @@ if __name__ == '__main__':
     run_name = f"run-{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
 
     train_df = labels_df.iloc[500:1000].copy()
-    val_df = labels_df.iloc[:500].copy()
+    val_df = labels_df.iloc[500:1000].copy()
 
 
     init_dataframe_view()
